d6502388823c0abbff1fbdaacb20789f7e85bceb
Steven <hensteve250@gmail.com>
2024-04-01 20:27:05 -0500

    next-con

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "home-style-nook.vercel.app", 
+             
 
-module.exports = nextConfig
+			},
+		],
+	},
+};
+
+module.exports = nextConfig;
