234c64999cd90acdb831c5c87100f219a860014f
Steven <hensteve250@gmail.com>
2024-04-08 16:24:48 -0500

    seguir probando

--- src/app/api/trpc/[trpc]/route.tsx
+++ src/app/api/trpc/[trpc]/route.tsx
@@ -1,16 +1,14 @@
-import { appRouter } from "@/trpc";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import type { NextRequest } from 'next/server';
+import { appRouter } from '@/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
-const handler = (req: NextRequest) => {
-    fetchRequestHandler({
-        endpoint: "/api/trpc",
-        req,
-        router: appRouter,
-        // @ts-expect-error  context already passed from express middleware
-        createContext: () => ({}),
-    })
+const handler = (req: Request) => {
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    // @ts-expect-error context already passed from express middleware
+    createContext: () => ({}),
+  })
 }
-
 
 export { handler as GET, handler as POST }
