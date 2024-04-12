971d8b5281fe20cc45c06e2a763d38b1cd2e475e
Steven <hensteve250@gmail.com>
2024-04-11 20:26:03 -0500

    act_mobilenav2

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,94 +1,128 @@
-import Link from 'next/link'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import { Icon } from "./Logo";
-import NavItems from './NavItems'
-import { buttonVariants } from './ui/button'
-import Cart from './Cart'
-import { getServerSideUser } from '@/lib/payload-utils'
-import { cookies } from 'next/headers'
-import UserAccountNav from './UserAccountNav'
-import MobileNav from './MobileNav'
+'use client'
 
-const Navbar = async () => {
-  const nextCookies = cookies()
-  const { user } = await getServerSideUser(nextCookies)
+import { PRODUCT_CATEGORIES } from '@/config'
+import { Menu, X } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const pathname = usePathname()
+
+  // whenever we click an item in the menu and navigate away, we want to close the menu
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathname])
+
+  // when we click the path we are currently on, we still want the mobile menu to close,
+  // however we cant rely on the pathname for it because that won't change (we're already there)
+  const closeOnCurrent = (href: string) => {
+    if (pathname === href) {
+      setIsOpen(false)
+    }
+  }
+
+  // remove second scrollbar when mobile menu is open
+  useEffect(() => {
+    if (isOpen)
+      document.body.classList.add('overflow-hidden')
+    else document.body.classList.remove('overflow-hidden')
+  }, [isOpen])
+
+  if (!isOpen)
+    return (
+      <button
+        type='button'
+        onClick={() => setIsOpen(true)}
+        className='lg:hidden relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400'>
+        <Menu className='h-6 w-6' aria-hidden='true' />
+      </button>
+    )
 
   return (
-    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
-      <header className='relative bg-white'>
-        <MaxWidthWrapper>
-          <div className='border-b border-gray-200'>
-            <div className='flex h-16 items-center'>
-              <MobileNav />
+    <div>
+      <div className='relative z-40 lg:hidden'>
+        <div className='fixed inset-0 bg-black bg-opacity-25' />
+      </div>
 
-              <div className='ml-4 flex lg:ml-0'>
-                <Link href='/'>
-                  <Icon.logo className='h-10 w-10' />
+      <div className='fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex'>
+        <div className='w-4/5'>
+          <div className='relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl'>
+            <div className='flex px-4 pb-2 pt-5'>
+              <button
+                type='button'
+                onClick={() => setIsOpen(false)}
+                className='relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400'>
+                <X className='h-6 w-6' aria-hidden='true' />
+              </button>
+            </div>
+
+            <div className='mt-2'>
+              <ul>
+                {PRODUCT_CATEGORIES.map((category) => (
+                  <li
+                    key={category.label}
+                    className='space-y-10 px-4 pb-8 pt-10'>
+                    <div className='border-b border-gray-200'>
+                      <div className='-mb-px flex'>
+                        <p className='border-transparent text-gray-900 flex-1 whitespace-nowrap border-b-2 py-4 text-base font-medium'>
+                          {category.label}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className='grid grid-cols-2 gap-y-10 gap-x-4'>
+                      {category.featured.map((item) => (
+                        <div
+                          key={item.name}
+                          className='group relative text-sm'>
+                          <div className='relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                            <Image
+                              fill
+                              src={item.imageSrc}
+                              alt='product category image'
+                              className='object-cover object-center'
+                            />
+                          </div>
+                          <Link
+                            href={item.href}
+                            className='mt-6 block font-medium text-gray-900'>
+                            {item.name}
+                          </Link>
+                        </div>
+                      ))}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className='space-y-6 border-t border-gray-200 px-4 py-6'>
+              <div className='flow-root'>
+                <Link
+                  onClick={() => closeOnCurrent('/sign-in')}
+                  href='/sign-in'
+                  className='-m-2 block p-2 font-medium text-gray-900'>
+                  Sign in
                 </Link>
               </div>
-
-              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
-                <NavItems />
-              </div>
-
-              <div className='ml-auto flex items-center'>
-                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
-                  {user ? null : (
-                    <Link
-                      href='/sign-in'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Sign in
-                    </Link>
-                  )}
-
-                  {user ? null : (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  )}
-
-                  {user ? (
-                    <UserAccountNav user={user} />
-                  ) : (
-                    <Link
-                      href='/sign-up'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Create account
-                    </Link>
-                  )}
-
-                  {user ? (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  ) : null}
-
-                  {user ? null : (
-                    <div className='flex lg:ml-6'>
-                      <span
-                        className='h-6 w-px bg-gray-200'
-                        aria-hidden='true'
-                      />
-                    </div>
-                  )}
-
-                  <div className='ml-4 flow-root lg:ml-6'>
-                    <Cart />
-                  </div>
-                </div>
+              <div className='flow-root'>
+                <Link
+                  onClick={() => closeOnCurrent('/sign-up')}
+                  href='/sign-up'
+                  className='-m-2 block p-2 font-medium text-gray-900'>
+                  Sign up
+                </Link>
               </div>
             </div>
           </div>
-        </MaxWidthWrapper>
-      </header>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default MobileNav
